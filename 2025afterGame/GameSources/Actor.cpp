a28f174f097ce2ebbde1dfcadca7d2a8fd2f300c
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2025-09-25 10:23:39 +0900

    Actorクラス、MyGameObjectクラス作成＆ファイル追加

---
+++ 2025afterGame/GameSources/Actor.cpp
@@ -0,0 +1,16 @@
+/*!
+@file Actor.cpp
+@brief ゲーム中の動く3Dオブジェクトの親クラス
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+#include "Actor.h"
+
+namespace basecross {
+	void Actor::OnCreate()
+	{
+
+	}
+}
+//end basecross
---
+++ 2025afterGame/GameSources/MyGameObject.h
@@ -0,0 +1,33 @@
+/*!
+@file MyGameObject.h
+@brief 全てのゲームのオブジェクトの元となるクラス
+*/
+
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	class MyGameObject :public GameObject
+	{
+	public:
+		MyGameObject(const shared_ptr<Stage>& stagePtr);
+		~MyGameObject();
+
+	private:
+
+	};
+
+	MyGameObject::MyGameObject(const shared_ptr<Stage>& stagePtr) :
+		GameObject(stagePtr)
+	{
+
+	}
+
+	MyGameObject::~MyGameObject()
+	{
+
+	}
+
+}
+//end basecross
