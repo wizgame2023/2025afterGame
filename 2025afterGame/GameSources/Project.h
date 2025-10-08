58468de006719800bd97ebe2b058ae056e5719a5
MSI\yuuta <yuuta3sanpei@gmail.com>
2025-10-08 15:59:04 +0900

    インクルードの整理をしました

--- 2025afterGame/GameSources/Project.h
+++ 2025afterGame/GameSources/Project.h
@@ -3,18 +3,40 @@
 @brief コンテンツ用のヘッダをまとめる
 */
 
+/*
+   インクルードルール
+   親クラスがMyGameObjectから近いクラス順に上の行に書いてください
+   ○〇クラスが親クラスというのをコメントでブロックを作るのでそれを見て追加お願いします
+*/
+
 #pragma once
 
 
 #include "ProjectShader.h"
 #include "ProjectBehavior.h"
 #include "Scene.h"
-#include "GameStage.h"
-#include "MultiViewStage.h"
 #include "Character.h"
-#include "Player.h"
+
+// 殿堂入りMyGameObject本体
+#include "MyGameObject.h";
+
+
+// 親クラスがMyGameObject//////////////////////////
+#include "Actor.h";
 #include "MainCamera.h"
 #include "AttackCollision.h"
+///////////////////////////////////////////////////
+
+// 親クラスがActor/////////////////////////////////
+#include "Player.h";
+#include "Bullet.h";
+#include "Barrier.h";
+///////////////////////////////////////////////////
+
+// ステージ関係は一番下のブロックでインクルードしてください
+#include "GameStage.h"
+#include "MultiViewStage.h"
+///////////////////////////////////////////////////
 
 
 
