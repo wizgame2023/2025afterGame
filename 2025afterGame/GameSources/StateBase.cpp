b2192833b8aa9d39e9300b2494e41445bded9729
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2025-10-15 14:41:16 +0900

    StateBaseとStateEnemyのファイル追加＆それによるインクルード追加

---
+++ 2025afterGame/GameSources/StateBase.cpp
@@ -0,0 +1,23 @@
+/*!
+@file StateBase.cpp
+@brief 基盤ステート
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+	StateBase::StateBase()
+	{
+
+	}
+
+	StateBase::~StateBase()
+	{
+
+	}
+
+}
+//end basecross
+
+
---
+++ 2025afterGame/GameSources/StateBase.h
@@ -0,0 +1,22 @@
+/*!
+@file ProjectBehavior.h
+@brief プロジェク定義の行動クラス
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	class StateBase
+	{
+	private:
+
+	public:
+		StateBase();
+		~StateBase();
+	};
+
+}
+
+//end basecross
+
