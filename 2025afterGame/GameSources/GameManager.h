a122c90edb8759600f7f20f55af7bfa82092c839
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2025-10-15 09:31:48 +0900

    デルタタイムの取得がゲームマネージャからでもできるようにしました

--- 2025afterGame/GameSources/GameManager.h
+++ 2025afterGame/GameSources/GameManager.h
@@ -24,6 +24,7 @@ namespace basecross {
 		static unique_ptr<GameManager, GameManagerDeleter> m_GameManager;
 
 		float m_deltaTime = 0.0f; // 次のフレームに移動するまでの時間
+		int m_gameStageNow = 0; // 現在のゲームステージの変数
 
 	public:
 		//構築と破棄
@@ -42,9 +43,19 @@ namespace basecross {
 		// 子マネージャー破棄処理
 		void DeleteChildManager();
 
+
 		//初期化
 		virtual void OnCreate();
-		virtual void OnUpdate();
+		virtual void OnUpdate();	
+		
+		// デルタタイムのゲッタ
+		float GetDeltaTime();
+
+		// gameStageNowのゲッタ
+		int GetGameStageNow();
+		// gameStageNowのセッタ
+		void SetGameStageNow(int gameStageNow);
+
 	};
 
 
