b81f4cb39591f7cfbd3efda6177c7b1889d5f9b8
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2025-10-28 14:37:06 +0900

    バリアステートの開始時ステート処理と終了時ステート処理の作成

--- 2025afterGame/GameSources/StateBarrier.cpp
+++ 2025afterGame/GameSources/StateBarrier.cpp
@@ -8,30 +8,132 @@
 #include "StateBarrier.h"
 
 namespace basecross {
-	StateBarrier::StateBarrier()
+	// バリアステートの元となるクラス----------------------------------------------------------------------------->
+	StateBarrierBase::StateBarrierBase(const shared_ptr<MyGameObject>& parentObj) :
+		StateBase(parentObj),
+		m_barrier(dynamic_pointer_cast<Barrier>(parentObj))
 	{
 
 	}
-	StateBarrier::~StateBarrier()
+	StateBarrierBase::~StateBarrierBase()
+	{
+
+	}
+	// 作成時処理
+	void StateBarrierBase::OnEnter()
+	{
+		StateBase::OnEnter();
+		m_deltaTime = GameManager::GetGameManager()->GetDeltaTime();
+		m_barrierLock = m_barrier.lock();
+	}
+	// 更新処理
+	void StateBarrierBase::OnUpdate()
+	{
+		m_deltaTime = GameManager::GetGameManager()->GetDeltaTime();
+		m_barrierLock = m_barrier.lock();
+	}
+	//----------------------------------------------------------------------------------------------------------->
+
+	// バリア展開時の拡大処理--------------------------------------------------------------------------------------->
+	StateBarrierStart::StateBarrierStart(const shared_ptr<MyGameObject>& parentObj):
+		StateBarrierBase(parentObj),
+		m_barrier(dynamic_pointer_cast<Barrier>(parentObj))
+	{
+
+	}
+	StateBarrierStart::~StateBarrierStart()
+	{
+
+	}
+	// 作成時処理
+	void StateBarrierStart::OnEnter()
+	{
+		// 親クラスEnter処理
+		StateBarrierBase::OnEnter();
+
+		// この時点での親オブジェクトのサイズ倍率を取得する
+		m_sizePercent = m_barrierLock->GetSizePercent();
+
+		// 親オブジェクトを見えるようにする
+		m_barrierLock->SetDrawActive(true);
+	}
+	// 更新
+	void StateBarrierStart::OnUpdate()
+	{// ここのm_sizePercentの数値が大きくなるタイミングがおかしいので注視すべし！
+		// 親クラスUpdate処理
+		StateBarrierBase::OnUpdate();
+
+		float delta = GameManager::GetGameManager()->GetDeltaTime();
+		m_sizePercent += delta;
+
+		// サイズが規定以上になったら拡大処理をやめる
+		if (m_sizePercent >= 1.0f)
+		{
+			m_sizePercent = 1.0f;
+
+			// デフォルトステートへ変更する
+			m_barrierLock->ChangeState(L"Default");
+		}
+
+		m_barrierLock->SetSizePercent(m_sizePercent);
+	}
+	// 終了時処理
+	void StateBarrierStart::OnExit()
+	{
+		auto test = 0;
+	}
+	//------------------------------------------------------------------------------------------------------------------------->
+
+	// バリア終了時の縮小処理ステート------------------------------------------------------------------------------------------>
+	StateBarrierEnd::StateBarrierEnd(const shared_ptr<MyGameObject>& parentObj):
+		StateBarrierBase(parentObj)
 	{
 
 	}
 
-	void StateBarrier::OnEnter()
+	StateBarrierEnd::~StateBarrierEnd()
 	{
 
 	}
 
-	void StateBarrier::OnUpdate()
+	// 開始時処理
+	void StateBarrierEnd::OnEnter()
 	{
+		// 親クラスEnter処理
+		StateBarrierBase::OnEnter();
 
+		// この時点での親オブジェクトのサイズ倍率を取得する
+		m_sizePercent = m_barrierLock->GetSizePercent();
 	}
 
-	void StateBarrier::OnExit()
+	// 更新処理
+	void StateBarrierEnd::OnUpdate()
 	{
+		// 親クラスUpdate処理
+		StateBarrierBase::OnUpdate();
 
+		m_sizePercent -= m_deltaTime;
+
+		// サイズが規定以下になったら縮小処理をやめる
+		if (m_sizePercent <= 0.0f)
+		{
+			m_sizePercent = 0.0f;
+
+			// 親オブジェクトを見えないようにする
+			m_barrierLock->SetDrawActive(false);
+
+			// デフォルトステートへ変更する
+			m_barrierLock->ChangeState(L"Default");
+		}
+
+		m_barrierLock->SetSizePercent(m_sizePercent);
 	}
 
-
+	// 終了時処理
+	void StateBarrierEnd::OnExit()
+	{
+		auto test = 0;
+	}
+	//------------------------------------------------------------------------------------------------------------------------>
 }
 //end basecross
--- 2025afterGame/GameSources/StateBarrier.h
+++ 2025afterGame/GameSources/StateBarrier.h
@@ -7,15 +7,67 @@
 #include "stdafx.h"
 
 namespace basecross {
-	class StateBarrier:public BaseState
+	// バリアステートのベースとなるクラス兼デフォルトステート
+	class StateBarrierBase :public StateBase
+	{
+	protected:
+		float m_sizePercent; // サイズの倍率
+		weak_ptr<Barrier> m_barrier; // 親オブジェクトとなるバリア
+		shared_ptr<Barrier> m_barrierLock; // 親オブジェクトをロック(存在していることを確認できた変数)
+
+	public:
+		StateBarrierBase(const shared_ptr<MyGameObject>& parentObj);
+		~StateBarrierBase();
+
+		void OnEnter() override;
+		void OnUpdate() override;
+		//void OnExit() override;
+	};
+
+
+
+	// バリア展開開始時の拡大ステート
+	class StateBarrierStart:public StateBarrierBase
+	{
+	private:
+		float m_sizePercent; // サイズの倍率
+		weak_ptr<Barrier> m_barrier; // 親オブジェクトとなるバリア
+
+	public:
+		StateBarrierStart(const shared_ptr<MyGameObject>& parentObj);
+		~StateBarrierStart();
+
+		void OnEnter() override;
+		void OnUpdate() override;
+		void OnExit() override;
+
+	};
+
+
+	// バリア終了時の縮小処理ステート
+	class StateBarrierEnd :public StateBarrierBase
 	{
 	public:
-		StateBarrier();
-		~StateBarrier();
+		StateBarrierEnd(const shared_ptr<MyGameObject>& parentObj);
+		~StateBarrierEnd();
 
-		void OnEnter()override;
-		void OnUpdate()override;
-		void OnExit()override;
+		void OnEnter() override;
+		void OnUpdate() override;
+		void OnExit() override;
+	};
+
+
+	// バリアのステートマシンの作成
+	class StateBarrierMachine :public StateMachineBase
+	{
+	public:
+		StateBarrierMachine(const shared_ptr<MyGameObject>& parentObj):
+			StateMachineBase(parentObj)
+		{
+			AddState(L"Start", shared_ptr<StateBarrierStart>(new StateBarrierStart(parentObj))); // 開始時の拡大処理
+			AddState(L"End", shared_ptr<StateBarrierEnd>(new StateBarrierEnd(parentObj))); // 終了時の縮小処理
+			AddState(L"Default", shared_ptr<StateBarrierBase>(new StateBarrierBase(parentObj))); // デフォルトステート
+		}
 
 	};
 }
