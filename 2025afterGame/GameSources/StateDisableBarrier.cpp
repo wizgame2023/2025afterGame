1e734c2e4e9b2b01662f37af0d2d3ccfc5387876
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2025-10-31 13:39:40 +0900

    バリア妨害のステート作成

--- 2025afterGame/GameSources/StateDisableBarrier.cpp
+++ 2025afterGame/GameSources/StateDisableBarrier.cpp
@@ -1,6 +1,7 @@
 /*!
 @file StateDisableBarrier.cpp
-@brief バリア無効化実体
+@brief バリア無効化ステート実体
+担当：三瓶裕太
 */
 
 #include "stdafx.h"
--- 2025afterGame/GameSources/StateDisableBarrier.h
+++ 2025afterGame/GameSources/StateDisableBarrier.h
@@ -9,6 +9,27 @@
 
 namespace basecross {
 
+	class StateDisableBarrier :public StateBase
+	{
+	private:
+		StateDisableBarrier(const shared_ptr<MyGameObject>& obj);
+		~StateDisableBarrier();
+
+		void OnEnter();
+		void OnUpdate();
+		void OnExit();
+	};
+
+	// ステートマシン
+	class StateMachineDisableBarrier :StateMachineBase
+	{
+	public:
+		StateMachineDisableBarrier(const shared_ptr<MyGameObject>& obj):
+			StateMachineBase(obj)
+		{
+			//AddState(L"Start",shared_ptr<StateDisableBarrier>(new StateDisableBarrier(obj)))
+		}
+	};
 }
 
 //end basecross
