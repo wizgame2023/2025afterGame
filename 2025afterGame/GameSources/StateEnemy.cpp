635bc551ac62b74bf985f23228ea2c06971aa69e
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2025-12-02 14:00:14 +0900

    敵のステートの必要最低限の空のステート作成

--- 2025afterGame/GameSources/StateEnemy.cpp
+++ 2025afterGame/GameSources/StateEnemy.cpp
@@ -7,7 +7,9 @@
 #include "Project.h"
 
 namespace basecross {
-	StateEnemy::StateEnemy()
+	StateEnemy::StateEnemy(const shared_ptr<MyGameObject>& parentObj) :
+		StateBase(parentObj),
+		m_enemy(dynamic_pointer_cast<Enemy>(parentObj))
 	{
 
 	}
@@ -16,6 +18,89 @@ namespace basecross {
 	{
 
 	}
+
+	void StateEnemy::OnEnter()
+	{
+		// 親クラスEnter処理
+		StateBase::OnEnter();
+
+		// 親オブジェクトが消えていないか確認する
+		m_enemyLock = m_enemy.lock();
+	}
+
+	void StateEnemy::OnUpdate()
+	{
+		// 親クラスUpdate処理
+		StateBase::OnUpdate();
+
+		// 親オブジェクトが消えていないか確認する
+		m_enemyLock = m_enemy.lock();
+
+		// 親オブジェクトが消えていたらエラーメッセージ
+		if (!m_enemyLock)
+		{
+			throw BaseException
+			{
+				L"親オブジェクトが存在しません",
+				L"if (!m_enemyLock)",
+				L"void StateEnemy::OnUpdate()"
+			};
+		}
+	}
+
+
+	// 追いかけるステート
+	StateTrackingEnemy::StateTrackingEnemy(const shared_ptr<MyGameObject>& parentObj):
+		StateEnemy(parentObj)
+	{
+
+	}
+
+	StateTrackingEnemy::~StateTrackingEnemy()
+	{
+
+	}
+
+	void StateTrackingEnemy::OnEnter()
+	{
+
+	}
+
+	void StateTrackingEnemy::OnUpdate()
+	{
+
+	}
+	//
+
+
+	// リスポーン待機ステート
+	StateRespawnEnemy::StateRespawnEnemy(const shared_ptr<MyGameObject>& parentObj):
+		StateEnemy(parentObj)
+	{
+
+	}
+
+	StateRespawnEnemy::~StateRespawnEnemy()
+	{
+
+	}
+
+	void StateRespawnEnemy::OnEnter()
+	{
+		// 親クラスEnter処理
+		StateEnemy::OnEnter();
+
+		// 自分が見えない状態に変更する
+	}
+
+	void StateRespawnEnemy::OnUpdate()
+	{
+		// 親クラスUpdate処理
+		StateEnemy::OnUpdate();
+
+		// リスポーン可能時間まで待機する
+	}
+	//
 
 }
 //end basecross
