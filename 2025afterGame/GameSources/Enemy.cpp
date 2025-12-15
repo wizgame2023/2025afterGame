e5782167ba17daf1a9a59ef8384a90f3e1442f53
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2025-12-15 11:48:19 +0900

    障害物を迂回するための処理(仮)

--- 2025afterGame/GameSources/Enemy.cpp
+++ 2025afterGame/GameSources/Enemy.cpp
@@ -299,9 +299,63 @@ namespace basecross {
 			// レイが当たったら動かないようにする
 			if (hitPos != Vec3(0.0f))
 			{
+				// ここを動かないようにじゃなくて迂回するルートを考える処理にする
 				m_moveVec = Vec3(0.0f);
+
+				//// 障害物を避けるために進むルートを決める
+				//auto obstaclesDodgeObj = DodgeRoute();
+				//// 追いかける対象を変える
+				//m_trackingObj = obstaclesDodgeObj;
 			}
 		}
+
+	}
+
+	// 障害物を避けるルートを考える処理
+	shared_ptr<TestCube> Enemy::DodgeRoute()
+	{
+		auto objVec = GetStage()->GetGameObjectVec();
+		vector<shared_ptr<TestCube>> m_obstaclesDodgeObjs;
+
+		for (auto obj : objVec)
+		{
+			// 回避ルート用のオブジェクトか確認した後回避ルートをきめる
+			auto obstaclesDodgeObj = dynamic_pointer_cast<TestCube>(obj);
+
+			if (obstaclesDodgeObj)
+			{
+				// 障害物を回避するための目印か確認出来たら障害物一覧として受け取る
+				if (obstaclesDodgeObj->FindTag(L"ObstaclesRoute"))
+				{
+					m_obstaclesDodgeObjs.push_back(obstaclesDodgeObj);
+				}
+			}
+		}
+
+		float min = 999999.9f; // 最短距離
+		shared_ptr<TestCube> targetObstaclesDodgeObj; // 障害物を避けるために追跡するオブジェクト
+
+		// 障害物を避けるためにどこを経由すると最短距離はどのオブジェクトか確認する
+		for (auto obj : m_obstaclesDodgeObjs)
+		{
+			auto objPos = obj->GetComponent<Transform>()->GetPosition();// 現在ここ作業中
+			Vec3 objAbsPos = Vec3(abs(objPos.x),abs(objPos.y),abs(objPos.z));
+			Vec3 myAbsPos = Vec3(abs(m_pos.x), abs(m_pos.y), abs(m_pos.z));
+			// 現在距離と障害物回避経由位置の距離の差を求める
+			float difference = abs(objAbsPos.x - myAbsPos.x) +
+				abs(objAbsPos.y - myAbsPos.y) +
+				abs(objAbsPos.z - myAbsPos.z);
+
+			// 最短距離だった場合そこを追跡対象として追いかける
+			if (min > difference && difference != 0.0f)
+			{
+				min = difference;
+				targetObstaclesDodgeObj = obj;
+			}
+		}
+
+		// 障害物を迂回するためにこのオブジェクトを追いかけてね
+		return targetObstaclesDodgeObj;
 	}
 
 	// 追いかける対象ポインタのゲッタ
--- 2025afterGame/GameSources/Enemy.h
+++ 2025afterGame/GameSources/Enemy.h
@@ -64,6 +64,9 @@ namespace basecross {
 		// 障害物を避ける処理
 		void DodgeObstacles(const Vec3& posPlayerDifference);
 
+		// 障害物を避けるルートを考える処理
+		shared_ptr<TestCube> DodgeRoute();
+
 		// 追いかける対象ポインタのゲッタ
 		shared_ptr<Actor> GetTrackingObj();
 	};
