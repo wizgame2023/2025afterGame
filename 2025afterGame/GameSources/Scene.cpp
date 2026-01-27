fce20d7a0bda1ab68080fbed7adc1a0cfe6b7fe2
UniverseSushi3319 <160200306+UniverseSushi3319@users.noreply.github.com>
2026-01-27 09:37:45 +0900

    時計を配置

--- 2025afterGame/GameSources/Scene.cpp
+++ 2025afterGame/GameSources/Scene.cpp
@@ -69,6 +69,9 @@ namespace basecross{
 		modelTex = modelPath + L"bed.png";
 		app->RegisterTexture(L"BedTex", modelTex);
 
+		modelTex = modelPath + L"Clock.png";
+		app->RegisterTexture(L"ClockTex", modelTex);
+
 		// Model /////////////////////////////////////////////////
 		// Player
 		auto modelMesh = MeshResource::CreateBoneModelMesh(modelPath, L"Player_Plane.bmf");
@@ -88,6 +91,9 @@ namespace basecross{
 
 		modelMesh = MeshResource::CreateStaticModelMesh(modelPath, L"tansu.bmf");
 		app->RegisterResource(L"Shelf", modelMesh);
+
+		modelMesh = MeshResource::CreateStaticModelMesh(modelPath, L"clock.bmf");
+		app->RegisterResource(L"Clock", modelMesh);
 
 		// UI /////////////////////////////////////////////////
 		auto numberSprite = uiPlayerPath + L"Number.png";
